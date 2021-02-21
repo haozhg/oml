e88bba74cc5187340815f625cee9edd1a8098a65
Hao Zhang <haozhang@alumni.princeton.edu>
2021-02-20 19:20:30 -0500

    Fix license in setup.py and republish to pypi (#5)
    * refactor, clean, packaging
    
    * use property instead of attributes
    
    * update docstring
    
    * renaming cleaning
    
    * black formatting
    
    * update tests
    
    * update plots
    
    * update demo
    
    * make demo works
    
    * update readme
    
    * update readme and renaming
    
    * rename and make lorenz work
    
    * update py notebook
    
    * add readme, cleanup
    
    * docstring, update readme
    
    * fix ~ in md
    
    * v++
    
    * better pkging
    
    * update pynb and add images
    
    * add image to readme
    
    * image side by side
    
    * change layout
    
    * add controlled case
    
    * align center
    
    * update readme
    
    * update readme
    
    * update license and v++
    
    * Merge master into dev (#4)
    
    * add Lorenz control showcase images to readme (#2)
    
    * refactor, clean, packaging
    
    * use property instead of attributes
    
    * update docstring
    
    * renaming cleaning
    
    * black formatting
    
    * update tests
    
    * update plots
    
    * update demo
    
    * make demo works
    
    * update readme
    
    * update readme and renaming
    
    * rename and make lorenz work
    
    * update py notebook
    
    * add readme, cleanup
    
    * docstring, update readme
    
    * fix ~ in md
    
    * v++
    
    * better pkging
    
    * update pynb and add images
    
    * add image to readme
    
    * image side by side
    
    * change layout
    
    * add controlled case
    
    * align center
    
    * update readme
    
    * update readme
    
    * change license to MIT
    
    * republish python pkg with MIT license (#3)
    
    * refactor, clean, packaging
    
    * use property instead of attributes
    
    * update docstring
    
    * renaming cleaning
    
    * black formatting
    
    * update tests
    
    * update plots
    
    * update demo
    
    * make demo works
    
    * update readme
    
    * update readme and renaming
    
    * rename and make lorenz work
    
    * update py notebook
    
    * add readme, cleanup
    
    * docstring, update readme
    
    * fix ~ in md
    
    * v++
    
    * better pkging
    
    * update pynb and add images
    
    * add image to readme
    
    * image side by side
    
    * change layout
    
    * add controlled case
    
    * align center
    
    * update readme
    
    * update readme
    
    * update license and v++
    
    * fix setup and v++

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r") as f:
 # This call to setup() does all the work
 setup(
     name="osysid",
-    version="0.1.3",
+    version="0.1.4",
     description="Efficient adaptive online linear/nonlinear model learning (system identification) and control",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/haozhg/osysid",
     author="Hao Zhang",
     author_email="haozhang@alumni.princeton.edu",
-    license="GNU",
+    license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
